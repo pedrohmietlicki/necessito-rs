d65bbad81a2b8b485b452fdca4931ceb46cb4473
Murilo Oliveira <murio.oliveira@dbseller.com.br>
2024-05-05 05:46:06 -0300

    fazendo primeira busca no firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCNfel4chJI-jBWQ63J6ebdx1mWfYGyvVM",
   authDomain: "necessito-rs.firebaseapp.com",
@@ -19,6 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export { app }
+export default app;
